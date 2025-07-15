38bf776ce5b24cecf2798530b13243aae8683d2d
Uğur Ekinci <ugurekinci@Ugur-MacBook-Air.local>
2025-07-15 15:56:33 +0300

    deneme.py updated.

--- deneme.py
+++ deneme.py
@@ -1,29 +1,39 @@
-from pypdf import PdfReader
-import os
+import subprocess
+import pymupdf4llm
+from pathlib import Path
+import pathlib
+import re
 
-def extract_text_from_pdf(file_path, text_name):
-    raw_text = []
-    with open(file_path, "rb") as f:
-        pdf = PdfReader(f)
-        for page_num in range(pdf.get_num_pages()):
-            page = pdf.get_page(page_num)
-            raw_text.append(page.extract_text())
-    text = "/n".join(raw_text)
     
-    with open(text_name, "w", encoding="utf-8") as f:
-        f.write(text)
-    
+def create_markdown_file(file_path : str, file_name: str, output=False):
+    markdown_text = pymupdf4llm.to_markdown(file_path)
+    if not file_name.endswith('.md'):
+        file_name += '.md'
+    Path(file_name).write_text(markdown_text, encoding="utf-8")
+    if output:
+        subprocess.run(["open", file_name], check=True)
+        return file_name
+    return "f{file_name} was created"
 
-def read_if_text_exist(file_name):
-    if os.path.exists(file_name):
-        with open(file_name, "r", encoding="utf-8") as f:
-            text = f.read()
-            return text
-    else:
-        return "No such file exists."
+def read_md(file_name):
+    file_path = Path(file_name)
+    if not file_path.exists():
+        raise FileNotFoundError(f"Dosya bulunamadı: {file_name}")
+    encodings = ['utf-8', 'latin-1', 'windows-1252']
+    for encoding in encodings:
+        try:
+            return Path(file_name).read_text(encoding=encoding)
+        except UnicodeDecodeError:
+            print(f"{encoding} ile dekode edilemedi, bir sonraki encoding deneniyor...")
+            
+    return None
     
 file_path = "/Users/ugurekinci/Documents/Naive_RAG_Pipeline/Naive-RAG-Pipeline/microsoft-annual-report.pdf"
-    
-extract_text = extract_text_from_pdf(file_path, "microsoft_annual_report.txt")
+c = [word for word in file_path.split("/") if re.search(".pdf", word)]
+file_name = c[0].replace(".pdf", ".md")
 
-read_text = read_if_text_exist("microsoft_annual_report.txt")
+create_markdown_doc = create_markdown_file(file_path, file_name, output=False)
+
+read_markdown_doc = read_md(file_name)
+
+print(read_markdown_doc)
